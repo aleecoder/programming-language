89b23f8cd9aa75dc878f6e1278c274973a94747f
HuanyuLee <hadoop_2020@163.com>
2022-10-02 21:04:45 +0800

    第一个scala程序

---
+++ src/class01_hello_world/Hello.scala
@@ -0,0 +1,11 @@
+package class01_hello_world
+
+/**
+ * @author HuanyuLee
+ * @date 2022/10/2
+ */
+object Hello {
+    def main(args: Array[String]): Unit = {
+        println("Hello World!")
+    }
+}
---
+++ src/class01_hello_world/JHello.java
@@ -0,0 +1,8 @@
+package class01_hello_world;
+
+/**
+ * @author HuanyuLee
+ * @date 2022/10/2
+ */
+public class JHello {
+}
