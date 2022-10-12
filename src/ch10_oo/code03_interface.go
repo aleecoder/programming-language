0210a64720c349bef43b08307c6bb3efc461c703
HuanyuLee <hadoop_2020@163.com>
2022-10-13 00:11:52 +0800

    接口

--- src/ch10_oo/code03_interface.go
+++ src/ch10_oo/code03_interface.go
@@ -1,9 +1,32 @@
 package main
 
-type greet interface {
+import "fmt"
+
+type Greet interface {
 	sayHello()
 }
 
-func main() {
+type Chinese struct {
+}
 
+func (c Chinese) sayHello() {
+	fmt.Println("你好！")
+}
+
+type American struct {
+}
+
+func (c American) sayHello() {
+	fmt.Println("Hello!")
+}
+
+func greet(g Greet) {
+	g.sayHello()
+}
+
+func main() {
+	c := Chinese{}
+	a := American{}
+	greet(c)
+	greet(a)
 }
