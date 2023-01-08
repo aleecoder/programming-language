d4664c5fb56b0c7538c0935be6300fd12ba9a5f1
aldalee <hadoop_2020@163.com>
2023-01-08 17:29:14 +0800

    modify main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+const int AMOUNT = 100;     //定义常量
+
 int main() {
+    int price = 0;
+    scanf("%d", &price);
     printf("Hello, World!\n");
+    printf("Your account balance is %d.", AMOUNT - price);
     return 0;
 }
