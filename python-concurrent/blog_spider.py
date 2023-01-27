bf3c479a496e50cba2abfd520fac9dab61abf16d
aldalee <hadoop_2020@163.com>
2023-01-27 17:10:47 +0800

    python实现生产者消费者爬虫

--- python-concurrent/blog_spider.py
+++ python-concurrent/blog_spider.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-urls = [f'https://www.cnblogs.com/#{page}' for page in range(1, 51)]
+urls = [f"https://www.cnblogs.com/#p{page}" for page in range(1, 51)]
 
 
 def craw(url):
@@ -12,11 +12,7 @@ def craw(url):
 
 def parse(html):
     """ 消费者 """
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'lxml')
     links = soup.find_all('a', class_='post-item-title')
     return [(link['href'], link.get_text()) for link in links]
 
-
-if __name__ == '__main__':
-    for res in parse(craw(urls[0])):
-        print(res)
