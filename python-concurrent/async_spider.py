6a90e370ef8f0270595dfcd3b408dd28d96108af
aldalee <hadoop_2020@163.com>
2023-01-28 13:59:59 +0800

    python异步IO实现并发爬虫

---
+++ python-concurrent/async_spider.py
@@ -0,0 +1,22 @@
+import asyncio
+import aiohttp
+import time
+from blog_spider import urls
+
+
+# 定义协程
+async def async_craw(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            result = await resp.text()
+            print(f"craw url:{url}, {len(result)}")
+
+
+loop = asyncio.get_event_loop()
+tasks = [loop.create_task(async_craw(url)) for url in urls]
+
+if __name__ == '__main__':
+    start = time.time()
+    loop.run_until_complete(asyncio.wait(tasks))
+    end = time.time()
+    print(f"use time:{end - start}seconds")
