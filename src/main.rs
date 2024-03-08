d53154beaf226a2029f50d8e80ac4f72df362771
aldalee <hadoop_2020@163.com>
2024-03-08 18:03:43 +0800

    rust thread safe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn f() {
+    let p = Arc::new(Mutex::new(Point { x: 0, y: 0 }));
+    let mut handles = Vec::new();
+    for i in 0..10 {
+        let cp = Arc::clone(&p);
+        let h = thread::spawn(move || {
+            let mut guard = cp.lock().unwrap();
+            match i % 2 {
+                0 => guard.x += 1,
+                _ => guard.y += 1,
+            }
+            println!("thread {} :: {} {}", i, guard.x, guard.y);
+        });
+        handles.push(h)
+    }
+    for handle in handles {
+        handle.join().unwrap()
+    }
+    let final_lock = p.lock().unwrap();
+    println!("threads done :: {} {}", final_lock.x, final_lock.y);
+}
+
 fn main() {
-    println!("Hello, world!");
+    f()
 }
